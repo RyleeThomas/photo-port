32794fd27f62b26a89bee38d40397a265c443c86
rylee thomas <ryleetreat@gmail.com>
2022-03-10 17:43:36 -0500

    added contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+const { name, email, message } = formState;
+
+function ContactForm() {
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    return (
+        <section>
+            <h1>Contact me</h1>
+            <form id='contact-form'>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type='text' defaultValue={name} onChange={handleChange} name="name"/>
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type='email' defaultValue={email} onChange={handleChange} name="email"/>
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <input type='message' defaultValue={message} onChange={handleChange} rows='5'/>
+                </div>
+                <button type="submit" onSubmit={handleSubmit} >submit</button>
+            </form>
+        </section>
+    );
+}
+    
+export default ContactForm;
